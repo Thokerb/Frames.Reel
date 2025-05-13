66bfaa2949f0760835caec8d8a34c0e3271bb361
Thokerb <Thomas.Kerber@student.uibk.ac.at>
2025-05-13 09:25:37 +0200

    first commit

---
+++ src/language/reel-validator.ts
@@ -0,0 +1,66 @@
+import type { Reference, ValidationAcceptor, ValidationChecks } from 'langium';
+import { type ReelAstType, type Variable, type VariableOverride } from './generated/ast.js';
+import type { ReelServices } from './reel-module.js';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: ReelServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.ReelValidator;
+    const checks: ValidationChecks<ReelAstType> = {
+        // AtomicModel: validator.checkPersonStartsWithCapital
+        VariableOverride: validator.checkVariableDeclaration
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class ReelValidator {
+
+    checkPersonStartsWithCapital(accept: ValidationAcceptor): void {
+        
+    }
+
+        checkVariableDeclaration(decl: VariableOverride, accept: ValidationAcceptor): void {
+        if (decl.ref && decl.value) {
+            const left = this.inferType(decl.ref);
+            const right = this.inferRightType(decl.value);
+
+            if(right === 'unknown' || left === 'unknown') { 
+                accept('error', `Type '${right}' is not assignable to type '${left}'.`, {
+                    node: decl,
+                    property: 'value'
+                });
+                return;
+            }
+
+            if (right !== left) {
+                accept('error', `Type '${right}' is not assignable to type '${left}'.`, {
+                    node: decl,
+                    property: 'value'
+                });
+                return;
+            }
+        }
+    }
+
+    inferRightType(node: string | number | boolean): string {
+        if (typeof node === 'string') {
+            return 'StringExpression';
+        } else if (typeof node === 'number') {
+            return 'IntegerExpression';
+        } else if (typeof node === 'boolean') {
+            return 'BooleanExpression';
+        }
+        return 'unknown';
+
+    }
+
+    inferType(node: Reference<Variable>): string {
+        return node.ref?.$type ?? 'unknown';
+    }
+
+}
---
+++ src/reel-scope.ts
@@ -0,0 +1,56 @@
+import {
+    ReferenceInfo, DefaultScopeProvider, Scope, AstNode,
+    Reference
+} from 'langium';
+import {  isStringExpression, isVariableOverride, State } from './language/generated/ast.js';
+
+export class ReelScopeProvider extends DefaultScopeProvider {
+    override getScope(context: ReferenceInfo): Scope {
+        // const container = context.container;
+        console.log(context);
+
+        // target element of member calls
+        if (context.property === 'ref' && isVariableOverride(context.container)) {
+
+            console.log(isStringExpression(context.container));
+
+            const memberCall = context.container;
+            const previous = memberCall.ref;
+            if (!previous) {
+                return super.getScope(context);
+            }
+            // get the state type
+            const stateType = memberCall.$container.$container.stateType;
+            return this.scopeClassMembers(stateType);
+            
+            // When the target of our member call isn't a class
+            // This means it is either a primitive type or a type resolution error
+            // Simply return an empty scope
+            // return EMPTY_SCOPE;
+        }
+        return super.getScope(context);
+    }
+
+    private scopeClassMembers(classItem: Reference<State>): Scope {
+    
+        console.log('ReelScopeProvider.scopeClassMembers2', classItem);
+        var allMembers: Array<AstNode> = [];
+
+        // add all properties of the class
+        if (classItem?.ref?.properties) {
+            allMembers = allMembers.concat(classItem.ref.properties.map((property) => <AstNode>{
+                $type: property.$type,
+                name: property.name,
+                $container: classItem.ref,
+                $containerRef: classItem,
+                $containerType: classItem?.ref?.$type,
+                $containerIndex: 0,
+                $containerProperty: 'properties',
+                $cstNode: property.$cstNode,
+                $document: property.$document,
+            }));
+        }
+
+        return this.createScopeForNodes(allMembers);
+    }
+}
