6c85f8452361e10ace5a7eafea4f38d501a01fdc
Thokerb <Thomas.Kerber@student.uibk.ac.at>
2025-08-08 09:53:40 +0200

    Enhance document change notifications in main-browser.ts

--- src/language/main-browser.ts
+++ src/language/main-browser.ts
@@ -1,7 +1,9 @@
-import { EmptyFileSystem } from 'langium';
+import {DocumentState, EmptyFileSystem} from 'langium';
 import { startLanguageServer } from 'langium/lsp';
 import { BrowserMessageReader, BrowserMessageWriter, createConnection } from 'vscode-languageserver/browser.js';
 import { createReelServices } from './reel-module.js';
+import {Diagnostic} from "vscode-languageserver-types";
+import {NotificationType} from "vscode-languageclient";
 
 declare const self: DedicatedWorkerGlobalScope;
 
@@ -10,6 +12,21 @@ const messageWriter = new BrowserMessageWriter(self);
 
 const connection = createConnection(messageReader, messageWriter);
 
-const { shared } = createReelServices({ connection, ...EmptyFileSystem });
+const { shared, Reel } = createReelServices({ connection, ...EmptyFileSystem });
 
 startLanguageServer(shared);
+
+// Send a notification with the serialized AST after every document change
+type DocumentChange = { uri: string, content: string, diagnostics: Diagnostic[] };
+const documentChangeNotification = new NotificationType<DocumentChange>('browser/DocumentChange');
+const jsonSerializer = Reel.serializer.JsonSerializer;
+shared.workspace.DocumentBuilder.onBuildPhase(DocumentState.Validated, documents => {
+	for (const document of documents) {
+		const json = jsonSerializer.serialize(document.parseResult.value);
+		connection.sendNotification(documentChangeNotification, {
+			uri: document.uri.toString(),
+			content: json,
+			diagnostics: document.diagnostics ?? []
+		});
+	}
+});
